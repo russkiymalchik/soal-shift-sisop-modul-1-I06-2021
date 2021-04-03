e91785877960273e21d50cada6b625332fc5ee11
hillmyhanif <73651220+hillmyhanif@users.noreply.github.com>
2021-04-03 17:22:19 +0700

    Delete soal3a.sh

--- soal 3/soal3a.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-
-for((i=1; i<=23; i++))
-do
-        wget -O Koleksi_$i https://loremflickr.com/320/240/kitten -a Foto.log
-done
-
-#remove duplicate in directory
-fdupes -d /home/hilmyhnf/Koleksi
