a3359c841cfe09416337f5ff93506a4df4536a7c
hillmyhanif <73651220+hillmyhanif@users.noreply.github.com>
2021-04-03 18:38:35 +0700

    Update soal3b.sh

--- soal 3/soal 3b/soal3b.sh
+++ soal 3/soal 3b/soal3b.sh
@@ -1,1 +1,16 @@
-#!bin/bash
+#!bit/bash
+
+# rerun soal3a.sh script
+bash soal3a.sh
+
+# set the destination directory
+destination="$(date +%d-%m-%Y)"
+
+# make the directory destination
+mkdir /home/hilmyhnf/$destination
+
+# move all Koleksi files to directory destination
+mv Koleksi_* /home/hilmyhnf/$destination
+
+# move Foto.log to directory destination
+mv Foto.log /home/hilmyhnf/$destination
