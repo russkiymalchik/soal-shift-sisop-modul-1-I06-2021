caee2db25426cbeead4468240dae6495f3400606
russkiymalchik <73766131+russkiymalchik@users.noreply.github.com>
2021-04-04 12:41:31 +0700

    Update 1a

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #1a.
-grep -i '[error|info].*' syslog.log
+cat syslog.log|grep -E -i '[error|info].*'|cut -d ":" -f4
 
 #1b.
 cat syslog.log|grep ERROR|cut -d ":" -f4|cut -d "(" -f1|sort|uniq -c
